24c9d8b7a99582f176c8853ea6e1f01344ac7e60
Giovanni <giovanni.gvaccaro@gmail.com>
2022-01-19 10:52:35 +0100

    schedulazione tipi doc

--- src/app/docserchdet/docserchdet.component.ts
+++ src/app/docserchdet/docserchdet.component.ts
@@ -16,11 +16,13 @@ export class DocserchdetComponent implements OnInit {
   });
 
   bar:boolean=false;
-  documenti!:any;
+  documentiCO!:any;
+  documentiBC!:any;
+  documentiBF!:any;
   showcat=false;
   intestazione:string[] = ['Data', 'numero', 'identificatore', 'IdCliente', 'IdFornitore', 'serie', 'categoriaMadreLivello1', 'nome', 'descrizione', 'Taglia', 'Colore', 'quantita', 'totaleDiRiga']
 
-  constructor(private msgkBar: MatSnackBar, private api:HttpClient) { }
+  constructor(private msgkBar: MatSnackBar, private api:HttpClient,private api2:HttpClient,private api3:HttpClient) { }
 
   ngOnInit(): void {
   }
@@ -33,9 +35,25 @@ export class DocserchdetComponent implements OnInit {
        var date = "d1="+this.range.value.start.toLocaleDateString("it-IT")+"&d2="+this.range.value.end.toLocaleDateString("it-IT");
       console.log(this.range.value.start.toLocaleDateString("it-IT"))
       this.bar=true;
-      this.api.get("https://cvggold-dash.ns0.it/json/dettagli/documenti.php?"+date).subscribe(
+      this.api.get("https://cvggold-dash.ns0.it/json/dettagli/documenti.php?"+date+"&tipo=CO").subscribe(
         data=>{
-          this.documenti = data;
+          this.documentiCO = data;
+          console.log(data);
+          this.bar = false;
+          this.showcat=true;
+        }
+      )
+      this.api2.get("https://cvggold-dash.ns0.it/json/dettagli/documenti.php?"+date+"&tipo=BC").subscribe(
+        data=>{
+          this.documentiBC = data;
+          console.log(data);
+          this.bar = false;
+          this.showcat=true;
+        }
+      )
+      this.api2.get("https://cvggold-dash.ns0.it/json/dettagli/documenti.php?"+date+"&tipo=BF").subscribe(
+        data=>{
+          this.documentiBF = data;
           console.log(data);
           this.bar = false;
           this.showcat=true;
